72293fb5313b499282f3815ea0a7bb42eee12c79
sabanggang12 <43600857+sabanggang12@users.noreply.github.com>
2018-09-28 23:56:08 +0700

    Add files via upload

---
+++ contohh.php
@@ -0,0 +1,27 @@
+<?php
+$a = "10";
+$b = "5";
+$c = "6";
+$d = "11";
+$e = "12";
+$f = "13";
+$g = "4";
+$h = "3";
+echo $a;echo " ~ ";echo $b;echo" ~ ";echo $a + $b / 2;
+echo "<br><br>";
+echo $a;echo " ~ ";echo $c;echo" ~ ";echo $a - $c - 0.5;
+echo "<br><br>";
+echo $a;echo " ~ ";echo $c;echo" ~ ";echo $a - $c - 0.5;echo" ~ ";echo 6.5 + 43.5;
+echo "<br><br>";
+echo $d;echo " ~ ";echo $c;echo" ~ ";echo $d - $c + 1.5;echo" ~ ";echo $d - $c + 101;
+echo "<br><br>";
+echo $d;echo " ~ ";echo $b;echo" ~ ";echo "-";echo $d - $b + 4.5;
+echo "<br><br>";
+echo $f;echo " ~ ";echo $g;echo" ~ ";echo "-";echo $f - $g - 2.5;
+echo "<br><br>";
+echo $f;echo " ~ ";echo $g;echo" ~ ";echo "-";echo $f - $g - 3.44;
+echo "<br><br>";
+echo $e;echo " ~ ";echo $h;echo" ~ ";echo "-";echo $e - $h - 3.44;
+echo "<br><br>";
+echo $d;echo " ~ ";echo $g;echo" ~ ";echo "-";echo $d - $g - 1.5;echo" ~ ";echo -5.5 + 11.5;
+?>
